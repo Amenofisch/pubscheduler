c083955667e0d5ca913ef7f81b80847fe6bf4fe2
amenofisch <me@amenofisch.dev>
2024-09-19 09:59:55 +0200

    update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,24 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'pubscheduler',
-    'description' => 'This plugin allows rescheduling the visibility of content elements and pages in Typo3.',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '12.4.0-12.4.99',
-        ],
+  'title' => 'pubscheduler',
+  'description' => 'This plugin allows rescheduling the visibility of content elements and pages in Typo3.',
+  'version' => '1.0.0',
+  'category' => 'misc',
+  'state' => 'stable',
+  'author' => 'amenofisch',
+  'author_email' => 'me@amenofisch.dev',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '12.4.0-12.4.99',
     ],
-    'autoload' => [
-        'psr-4' => [
-            'Amenofisch\\Pubscheduler\\' => 'Classes/',
-        ],
+    'conflicts' => [],
+    'suggests' => [],
+  ],
+  'autoload' => [
+    'psr-4' => [
+      'Amenofisch\\Pubscheduler\\' => 'Classes/',
     ],
+  ],
 ];
+
